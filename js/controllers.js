03ec5354f301e1768254c62ee1a78426af7912df
Rein Pettai <reinpettai@gmail.com>
2015-11-14 15:21:15 +0200

    Header update

--- js/controllers.js
+++ js/controllers.js
@@ -36,25 +36,10 @@ eatsyApp.controller('feedController', function ($scope) {
         'friends' : 'Bess, Karie, Elvira and 3 other friends have rated this place.'},
         {'id' : '4', 'name' : 'Truffe', 'logo' : 'img/restoranide-ikoonid/Truffle.png', 'dst' : '300m',
         'desc' : 'Neque porro quisquam est qui dolorem ipsum quia', 
-        'friends' : 'Bess, Karie, Elvira and 3 other friends have rated this place.'},
-        {'id' : '5', 'name' : 'McDonalds', 'logo' : 'img/restoranide-ikoonid/McDonalds.png', 'dst' : '300m',
-        'desc' : 'Neque porro quisquam est qui dolorem ipsum quia', 
-        'friends' : 'Bess, Karie, Elvira and 3 other friends have rated this place.'},
-        {'id' : '6', 'name' : 'Restaurant Piparmünt', 'logo' : 'img/restoranide-ikoonid/feed_icon2.png', 'dst' : '300m',
-        'desc' : 'Neque porro quisquam est qui dolorem ipsum quia', 
-        'friends' : 'Bess, Karie, Elvira and 3 other friends have rated this place.'},
-        {'id' : '4', 'name' : 'Truffe', 'logo' : 'img/restoranide-ikoonid/Truffle.png', 'dst' : '300m',
-        'desc' : 'Neque porro quisquam est qui dolorem ipsum quia', 
-        'friends' : 'Bess, Karie, Elvira and 3 other friends have rated this place.'},
-        {'id' : '5', 'name' : 'McDonalds', 'logo' : 'img/restoranide-ikoonid/McDonalds.png', 'dst' : '300m',
-        'desc' : 'Neque porro quisquam est qui dolorem ipsum quia', 
-        'friends' : 'Bess, Karie, Elvira and 3 other friends have rated this place.'},
-        {'id' : '6', 'name' : 'Restaurant Piparmynt', 'logo' : 'img/restoranide-ikoonid/feed_icon2.png', 'dst' : '300m',
-        'desc' : 'Neque porro quisquam est qui dolorem ipsum quia', 
         'friends' : 'Bess, Karie, Elvira and 3 other friends have rated this place.'}
     ];
 
-    $scope.feedItems.splice(1, 0, data);
+    // $scope.feedItems.splice(1, 0, data); <- Injection
 });
 
 eatsyApp.controller('friendsListController', function ($scope) {
@@ -63,7 +48,8 @@ eatsyApp.controller('friendsListController', function ($scope) {
         {'profile' : 'img/friends/ethan.png'},
         {'profile' : 'img/friends/jussi.png'},
         {'profile' : 'img/friends/pille.png'},
-        {'profile' : 'img/friends/rein.png'}
+        {'profile' : 'img/friends/rein.png'},
+        {'profile' : 'img/friends/hemant.png'}
    ]; 
 });
 
