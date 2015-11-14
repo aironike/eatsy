150d7e63e8419cacd73b9e28309379b402b1c43a
Rein Pettai <reinpettai@gmail.com>
2015-11-14 14:17:56 +0200

    Feed friends template

--- js/controllers.js
+++ js/controllers.js
@@ -22,3 +22,17 @@ eatsyApp.controller('restaurantController', function($scope) {
     $scope.message = 'restaurantPage';
     $scope.pageClass = "restaurant";
 });
+
+eatsyApp.controller('feedController', function ($scope) {
+    $scope.feedItems = [
+        {'id' : '1', 'name' : 'Restaurant Noa', 'logo' : 'img/icons/feed_icon.png', 'dst' : '300m',
+        'desc' : 'Neque porro quisquam est qui dolorem ipsum quia', 
+        'friends' : 'Bess, Karie, Elvira and 3 other friends have rated this place.'},
+        {'id' : '2', 'name' : 'Cafe Lyon', 'logo' : 'img/icons/feed_icon.png', 'dst' : '300m',
+        'desc' : 'Neque porro quisquam est qui dolorem ipsum quia', 
+        'friends' : 'Bess, Karie, Elvira and 3 other friends have rated this place.'},
+        {'id' : '3', 'name' : 'Cafe Lyon', 'logo' : 'img/icons/feed_icon.png', 'dst' : '300m',
+        'desc' : 'Neque porro quisquam est qui dolorem ipsum quia', 
+        'friends' : 'Bess, Karie, Elvira and 3 other friends have rated this place.'}
+    ];
+});
